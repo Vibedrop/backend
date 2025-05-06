e2de37a3b201c708f947dda8da0b8bc73e93f60c
David Jonsson <david.jonsson@chasacademy.se>
2025-05-06 16:19:28 +0000

    Multer mw

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,134 +1,10 @@
 import type { Response } from "express";
 import { z } from "zod";
 import { prisma } from "../utilities/prisma";
-import { nanoid } from "nanoid";
 import { ProtectedRequest } from "../middleware/authMiddleware";
 
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
 const ALLOWED_MIME_TYPES = ["audio/wave", "audio/wav", "audio/x-wav"];
-
-export async function getProject(req: ProtectedRequest, res: Response) {
-    const parsedProject = z
-        .object({
-            projectId: z.string(),
-        })
-        .safeParse(req.body);
-
-    if (!parsedProject.success) {
-        res.status(400).json({
-            message: "Bad request",
-        });
-        return;
-    }
-    const project = await prisma.project.findUnique({
-        where: {
-            id: parsedProject.data.projectId,
-        },
-        include: {
-            collaborators: {
-                include: {
-                    user: true,
-                },
-            },
-            owner: true, // this includes the project owner (User)
-            audioFiles: true,
-        },
-    });
-    res.status(200).json(project);
-}
-
-export async function uploadFile(req: ProtectedRequest, res: Response) {
-    // Validate user
-    const parsedUser = z
-        .object({
-            id: z.string(),
-            email: z.string().email(),
-        })
-        .safeParse(req.user);
-
-    if (!parsedUser.success) {
-        res.status(400).json({
-            message: "Bad request",
-        });
-        return;
-    }
-
-    // Validate request parameters
-    const parsedParams = z
-        .object({
-            projectId: z.string(),
-        })
-        .safeParse(req.params);
-
-    // Validate request body
-    const parsedBody = z
-        .object({
-            fileName: z.string().min(1),
-        })
-        .safeParse(req.body);
-
-    // Validate request file
-    const parsedFile = z
-        .object({
-            fieldname: z.string(),
-            originalname: z.string(),
-            encoding: z.string(),
-            mimetype: z
-                .string()
-                .refine(val => ALLOWED_MIME_TYPES.includes(val)),
-            buffer: z.instanceof(Buffer),
-            size: z.number().max(MAX_FILE_SIZE),
-        })
-        .safeParse(req.file);
-
-    // Check if all validations passed
-    if (!parsedFile.success || !parsedParams.success || !parsedBody.success) {
-        res.status(400).json({
-            error: "Bad request",
-        });
-        return;
-    }
-
-    // Check if the project exists and the user is the owner
-    const project = await prisma.project.findUnique({
-        where: {
-            id: parsedParams.data.projectId,
-            ownerId: parsedUser.data.id,
-        },
-    });
-
-    if (!project) {
-        res.status(404).json({
-            message: "Project not found",
-        });
-        return;
-    }
-
-    // Generate a unique s3 key
-    const key = nanoid(21);
-
-    // TODO: Upload the file to S3
-
-    // Create a db record for the audio file
-    try {
-        await prisma.audioFile.create({
-            data: {
-                name: parsedBody.data.fileName,
-                s3Key: key,
-                projectId: parsedParams.data.projectId,
-            },
-        });
-    } catch {
-        res.status(500).json({
-            message: "Internal server error",
-        });
-        return;
-    }
-
-    res.status(200).json({
-        message: "File uploaded successfully",
-    });
-}
 
 // Skapa nytt projekt
 export async function createProject(req: ProtectedRequest, res: Response) {
@@ -180,6 +56,37 @@ export async function createProject(req: ProtectedRequest, res: Response) {
     }
 }
 
+// Hämta projekt
+export async function getProject(req: ProtectedRequest, res: Response) {
+    const parsedProject = z
+        .object({
+            projectId: z.string(),
+        })
+        .safeParse(req.body);
+
+    if (!parsedProject.success) {
+        res.status(400).json({
+            message: "Bad request",
+        });
+        return;
+    }
+    const project = await prisma.project.findUnique({
+        where: {
+            id: parsedProject.data.projectId,
+        },
+        include: {
+            collaborators: {
+                include: {
+                    user: true,
+                },
+            },
+            owner: true, // this includes the project owner (User)
+            audioFiles: true,
+        },
+    });
+    res.status(200).json(project);
+}
+
 // Radera ett projekt
 export const deleteProject = async (req: ProtectedRequest, res: Response) => {
     const userId = req.user?.id;
@@ -209,3 +116,37 @@ export const deleteProject = async (req: ProtectedRequest, res: Response) => {
         res.status(500).json({ message: "Error deleting project" });
     }
 };
+
+// Ladda upp en ljudfil
+export async function uploadFile(req: ProtectedRequest, res: Response) {
+    const userId = req.user?.id;
+    const { projectId } = req.params;
+    const name = req.body.fileName;
+    const s3Key = req.body.s3Key;
+
+    if (!userId) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+
+    // Create a db record for the audio file
+    // TODO: Handle duplicates and possibly move to audioRoutes/controllers
+    try {
+        await prisma.audioFile.create({
+            data: {
+                name: name,
+                s3Key: s3Key,
+                projectId: projectId,
+            },
+        });
+    } catch {
+        res.status(500).json({
+            message: "Internal server error",
+        });
+        return;
+    }
+
+    res.status(200).json({
+        message: "File uploaded successfully",
+    });
+}
--- src/routes/audioRoutes.ts
+++ src/routes/audioRoutes.ts
@@ -1,26 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
 import { uploadAudio } from "../controllers/audioController";
-import multer from "multer";
-import multerS3 from "multer-s3";
+import Upload from "../middleware/multer";
 import { s3 } from "../utilities/s3";
 
 const router = Router();
 
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.S3_BUCKET_NAME || "",
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-        },
-        key: function (req, file, cb) {
-            cb(null, `audio/${Date.now().toString()}-${file.originalname}`);
-        },
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-    }),
-});
-
-router.post("/", authMiddleware, upload.single("file"), uploadAudio);
+router.post("/", authMiddleware, Upload.single("file"), uploadAudio);
 
 export default router;

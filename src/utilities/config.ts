a7dfaca83425b55c72ff35e9c42c4f2283e562b4
Cristian Pencheff <cribe@self-made.se>
2025-10-21 21:24:13 +0200

    Update production frontend URL

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -1,4 +1,4 @@
-const PROD_FRONTEND_URL = "https://vibedrop-frontend.cc25.chasacademy.dev";
+const PROD_FRONTEND_URL = "https://incandescent-platypus-3fdb48.netlify.app";
 const DEV_FRONTEND_URL = "http://localhost:3001"; // NextJS port
 
 export const FRONTEND_URL =
